5743b687118a80a3f8eade4515859181dfb9fe39
bberak <bberak@gmail.com>
2020-01-14 20:44:05 +1100

    Added another synth sound (electro robot or something)

---
+++ examples/synth-03.js
@@ -0,0 +1,19 @@
+const { synthesizer, loop, compose, map, scale } = require("../core");
+const { a, b, c, d, e, f, g } = require("../waves");
+const { lowPass } = require("../filters");
+const { listenForExit } = require("../utils");
+
+synthesizer(
+	compose(
+		loop(
+			[
+				[g(2), c(5), e(5), d(3), b(5), a(6), b(4), f(2), e(5)],
+				[g(2), c(5), e(5), d(3), b(5), a(6), b(4), f(2), e(5)],
+			],
+			875 // beats per minute
+		),
+		map(lowPass("l")(440), lowPass("r")(440))
+	)
+).play();
+
+listenForExit();
